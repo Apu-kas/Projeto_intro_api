309de535249e20ce6b19a13ab8964db92cced757
Yago Cândido <displaypanda@gmail.com>
2025-09-03 17:59:00 -0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY" , "wcc@2023")
-    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL" , "postgresql://postgres:SECRET_KEY@127.0.0.1:5432/tarefas_3b")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
