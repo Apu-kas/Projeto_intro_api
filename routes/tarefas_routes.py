a18c432b074d20ba6f148d51d03f4123d973afeb
Yago Cândido <displaypanda@gmail.com>
2025-09-03 17:58:31 -0300

    Delete routes directory

--- routes/tarefas_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request, jsonify
-from controllers.tarefa_controller import TarefaController
-
-tarefas_bp = Blueprint('tarefas', __name__)
-
-@tarefas_bp.route('/api/tarefas' , methods=['GET'])
-def get_listar_tarefas():
-    return jsonify([tarefa.to_dict() for tarefa in TarefaController.get_listar_tarefas()])
-
